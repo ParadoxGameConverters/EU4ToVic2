76cddb424b58054f21be9c55b8ed44d621139d55
idhrendur <idhrendur@hotmail.com>
2013-12-24 13:34:19 -0800

    Move CK2 To EU3 to subfolder

--- CopyUIFiles.bat
+++
@@ -1,3 +0,0 @@
-copy "Converter.UI.exe" "..\..\..\release\Converter.UI.exe"
-copy "..\..\UserPreferences.xml" "..\..\..\release\UserPreferences.xml"
-copy "..\..\Configuration.xml" "..\..\..\release\Configuration.xml"
