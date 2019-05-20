d1a81ce144440ae016ed31fea3a57493af73d70d
Idhrendur <idhrendur@hotmail.com>
2019-05-19 19:15:21 -0700

    Replace slaveCultureMapper with an instance of cultureMapper

--- EU4toV2/Source/EU4World/World.cpp
+++ EU4toV2/Source/EU4World/World.cpp
@@ -32,7 +32,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 #include "EU4Religion.h"
 #include "Regions/Areas.h"
 #include "../Configuration.h"
-#include "../Mappers/CultureMapper.h"
 #include "../Mappers/ProvinceMapper.h"
 #include "../Mappers/ReligionMapper.h"
 #include "Log.h"
