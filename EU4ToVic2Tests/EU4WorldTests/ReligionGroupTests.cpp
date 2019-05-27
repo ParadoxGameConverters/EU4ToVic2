e15a22b389d3e98dc812291957ec2498f343df13
Idhrendur <idhrendur@hotmail.com>
2019-05-27 10:08:14 -0700

    Move the new EU4 religion classes into their own folder.

--- EU4ToVic2Tests/EU4WorldTests/ReligionGroupTests.cpp
+++ EU4ToVic2Tests/EU4WorldTests/ReligionGroupTests.cpp
@@ -22,7 +22,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 
 #include "gtest/gtest.h"
-#include "../EU4toV2/Source/EU4World/ReligionGroup.h"
+#include "../EU4toV2/Source/EU4World/Religions/ReligionGroup.h"
 #include <sstream>
 
 
--- EU4ToVic2Tests/EU4WorldTests/ReligionTests.cpp
+++ EU4ToVic2Tests/EU4WorldTests/ReligionTests.cpp
@@ -22,7 +22,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 
 #include "gtest/gtest.h"
-#include "../EU4toV2/Source/EU4World/Religion.h"
+#include "../EU4toV2/Source/EU4World/Religions/Religion.h"
 
 
 
