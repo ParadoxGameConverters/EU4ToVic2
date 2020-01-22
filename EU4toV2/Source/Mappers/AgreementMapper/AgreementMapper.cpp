b71f79eac5d7aff0923ff0fbf297e4d8585a7253
Ivo Elezovic <ivo@elezovic.su>
2020-01-22 18:25:13 +0100

    travis patch

--- EU4toV2/Source/Mappers/AgreementMapper/AgreementMapper.cpp
+++ EU4toV2/Source/Mappers/AgreementMapper/AgreementMapper.cpp
@@ -22,32 +22,27 @@ void mappers::AgreementMapper::registerKeys()
 	registerKeyword("colonies", [this](const std::string& unused, std::istream& theStream)
 		{
 			commonItems::stringList subjectList(theStream);
-			auto theList = subjectList.getStrings();
-			colonies = std::set(theList.begin(), theList.end());
+			for (const auto& subject : subjectList.getStrings()) colonies.insert(subject);
 		});
 	registerKeyword("onesiders", [this](const std::string& unused, std::istream& theStream)
 		{
 			commonItems::stringList subjectList(theStream);
-			auto theList = subjectList.getStrings();
-			onesiders = std::set(theList.begin(), theList.end());
+			for (const auto& subject : subjectList.getStrings()) onesiders.insert(subject);
 		});
 	registerKeyword("doublesiders", [this](const std::string& unused, std::istream& theStream)
 		{
 			commonItems::stringList subjectList(theStream);
-			auto theList = subjectList.getStrings();
-			doublesiders = std::set(theList.begin(), theList.end());
+			for (const auto& subject : subjectList.getStrings()) doublesiders.insert(subject);
 		});
 	registerKeyword("tributaries", [this](const std::string& unused, std::istream& theStream)
 		{
 			commonItems::stringList subjectList(theStream);
-			auto theList = subjectList.getStrings();
-			tributaries = std::set(theList.begin(), theList.end());
+			for (const auto& subject : subjectList.getStrings()) tributaries.insert(subject);
 		});
 	registerKeyword("vassals", [this](const std::string& unused, std::istream& theStream)
 		{
 			commonItems::stringList subjectList(theStream);
-			auto theList = subjectList.getStrings();
-			vassals = std::set(theList.begin(), theList.end());
+			for (const auto& subject : subjectList.getStrings()) vassals.insert(subject);
 		});
 	registerRegex("[a-zA-Z0-9\\_.:]+", commonItems::ignoreItem);
 }
