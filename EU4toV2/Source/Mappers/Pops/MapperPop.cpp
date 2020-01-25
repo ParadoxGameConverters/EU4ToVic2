25b9bd348eac792b60598b28a7d0b0a60d004bc5
Ivo Elezovic <ivo@elezovic.su>
2020-01-25 17:35:59 +0100

    travis patch

--- EU4toV2/Source/Mappers/Pops/MapperPop.cpp
+++ EU4toV2/Source/Mappers/Pops/MapperPop.cpp
@@ -16,7 +16,7 @@ mappers::MapperPop::MapperPop(std::istream& theStream)
 	registerKeyword("size", [this](const std::string& unused, std::istream& theStream)
 		{
 			commonItems::singleDouble sizeDbl(theStream);
-			size = lround(sizeDbl.getDouble());
+			size = static_cast<int>(sizeDbl.getDouble());
 		});
 	registerRegex("[a-zA-Z0-9\\_.:]+", commonItems::ignoreItem);
 
