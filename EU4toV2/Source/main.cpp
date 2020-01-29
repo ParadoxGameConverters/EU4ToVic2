93ebfed63cbf9ab3381bf0414f99fc78fe826f8a
Ivo Elezovic <ivo@elezovic.su>
2020-01-29 19:13:56 +0100

    Don't die on input but work with defaults.

--- EU4toV2/Source/main.cpp
+++ EU4toV2/Source/main.cpp
@@ -10,10 +10,13 @@ int main(const int argc, const char * argv[])
 		LOG(LogLevel::Info) << versionParser;
 		LOG(LogLevel::Info) << "Built on " << __TIMESTAMP__;
 		if (argc >= 2)
-		{			
-			LOG(LogLevel::Error) << "EU4toVic2 no longer takes savegame location as a parameter.";
-			LOG(LogLevel::Error) << "Please place the correct path to your save game in configuration.txt";
-			return 0;
+		{
+			std::string argv1 = argv[1];
+			if (argv1 != "input.eu4")
+			{
+				LOG(LogLevel::Info) << "EU4toVic2 no longer takes savegame path as a parameter";
+				LOG(LogLevel::Info) << "It uses configuration.txt, configured manually or by the frontend.";
+			}
 		}
 		convertEU4ToVic2(versionParser);
 		return 0;
