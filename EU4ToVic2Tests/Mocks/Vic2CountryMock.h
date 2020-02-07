364066f8b08a0a80b4ad28a1a09a8b24e6fd01fe
Ivo Elezovic <ivo@elezovic.su>
2020-02-07 14:58:30 +0100

    style bits

--- EU4ToVic2Tests/Mocks/Vic2CountryMock.h
+++ EU4ToVic2Tests/Mocks/Vic2CountryMock.h
@@ -1,30 +1,9 @@
-/*Copyright (c) 2019 The Paradox Game Converters Project
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
-
+#ifndef VIC2_COUNTRY_MOCK_H
+#define VIC2_COUNTRY_MOCK_H
 
 #include "gmock/gmock.h"
 #include "../../EU4toV2/Source/EU4World/Country/EU4Country.h"
 #include "../../EU4toV2/Source/V2World/Country/Country.h"
-
 
 class mockVic2Country: public V2::Country
 {
@@ -33,3 +12,5 @@ class mockVic2Country: public V2::Country
 		MOCK_CONST_METHOD0(getProvinces, const std::map<int, std::shared_ptr<V2::Province>>&());
 		MOCK_CONST_METHOD0(getSourceCountry, std::shared_ptr<EU4::Country>());
 };
+
+#endif // VIC2_COUNTRY_MOCK_H
