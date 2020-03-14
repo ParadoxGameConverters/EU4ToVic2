e9cc8abaaf1ed53ae16ec0ecd013280c82487b47
Ivo Elezovic <ivo@elezovic.su>
2020-03-14 02:23:26 +0100

    country was unhappy.

--- EU4toV2/Source/EU4World/Country/EU4Country.h
+++ EU4toV2/Source/EU4World/Country/EU4Country.h
@@ -7,7 +7,6 @@
 #include "../../Mappers/IdeaEffects/IdeaEffectMapper.h"
 #include "newParser.h"
 #include <memory>
-#include <optional>
 #include <set>
 #include "EU4NationalSymbol.h"
 #include "../EU4Version.h"
@@ -35,7 +34,12 @@ namespace EU4
 			const mappers::IdeaEffectMapper& ideaEffectMapper, 
 			const mappers::CultureGroups& cultureGroupsMapper
 		);
-
+		virtual ~Country() = default;
+		Country(const Country& source) = default;
+		Country& operator=(const Country&) = default;
+		Country(Country&&) = default;
+		Country& operator=(Country&&) = default;
+		
 		void readFromCommonCountry(const std::string& fileName, const std::string& fullFilename); // Add any additional information available from the specified country file.
 		void setLocalisationName(const std::string& language, const std::string& name);
 		void setLocalisationAdjective(const std::string& language, const std::string& adjective);
@@ -71,11 +75,11 @@ namespace EU4
 		[[nodiscard]] virtual double getAdmTech() const { return admTech; }
 		[[nodiscard]] virtual double getDipTech() const { return dipTech; }
 		[[nodiscard]] virtual double getMilTech() const { return milTech; }
-		[[nodiscard]] auto getArmy() const { return army; }
-		[[nodiscard]] auto getNavy() const { return navy; }
-		[[nodiscard]] auto getCommerce() const { return commerce; }
-		[[nodiscard]] auto getIndustry() const { return industry; }
-		[[nodiscard]] auto getCulture() const { return culture; }
+		[[nodiscard]] virtual double getArmy() const { return army; }
+		[[nodiscard]] virtual double getNavy() const { return navy; }
+		[[nodiscard]] virtual double getCommerce() const { return commerce; }
+		[[nodiscard]] virtual double getIndustry() const { return industry; }
+		[[nodiscard]] virtual double getCulture() const { return culture; }
 		[[nodiscard]] auto getSlavery() const { return slavery; }
 		[[nodiscard]] auto getUpper_house_composition() const { return upper_house_composition; }
 		[[nodiscard]] auto getVote_franchise() const { return vote_franchise; }
