bf74293ef99b360613538748b2055d2175f4e5a2
Greg Bischoff <63864730+GregB76@users.noreply.github.com>
2020-05-12 22:59:47 +0300

    Update build_EU4ToVic2_release.sh

--- build_EU4ToVic2_release.sh
+++ build_EU4ToVic2_release.sh
@@ -7,8 +7,8 @@ rm -rf "EU4toV2-$version.tar.bz2"
 cd EU4toV2
 rm -rf build
 rm -rf Release-Linux
-cmake -H. -Bbuild
-cmake --build build -- -j3
+cmake -H. -Bbuild -DCMAKE_C_COMPILER=/usr/local/bin/gcc-8 -DCMAKE_CXX_COMPILER=/usr/local/bin/g++-8 -DCMAKE_EXE_LINKER_FLAGS=-liconv
+cmake --build build -- -j3 
 cp -rf "Release-Linux" "../EU4toV2-$version"
 cd ..
 
