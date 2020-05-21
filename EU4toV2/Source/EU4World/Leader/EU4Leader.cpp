2d235ff84256b760781a15f440b3b3b4c4565853
Ivo Elezović <ivo@elezovic.su>
2020-05-21 09:07:26 +0200

    No female leaders. (#502)

--- EU4toV2/Source/EU4World/Leader/EU4Leader.cpp
+++ EU4toV2/Source/EU4World/Leader/EU4Leader.cpp
@@ -5,56 +5,46 @@
 
 EU4::Leader::Leader(std::istream& theStream)
 {
-	registerKeyword("name", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleString nameString(theStream);
-			name = nameString.getString();
-		});
-	registerKeyword("type", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleString typeString(theStream);
-			leaderType = typeString.getString();
-		});
-	registerKeyword("female", [this](const std::string& unused, std::istream& theStream)
-		{
-			commonItems::ignoreItem(unused, theStream);
-			female = true;
-		});
-	registerKeyword("manuever", [this](const std::string& unused, std::istream& theStream)
-		{ // don't fix PDX typo!
-			const commonItems::singleInt theValue(theStream);
-			maneuver = theValue.getInt();
-		});
-	registerKeyword("fire", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleInt theValue(theStream);
-			fire = theValue.getInt();
-		});
-	registerKeyword("shock", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleInt theValue(theStream);
-			shock = theValue.getInt();
-		});
-	registerKeyword("siege", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleInt theValue(theStream);
-			siege = theValue.getInt();
-		});
-	registerKeyword("country", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleString countryString(theStream);
-			country = countryString.getString();
-		});
-	registerKeyword("activation", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleString dateString(theStream);
-			activationDate = date(dateString.getString());
-		});
-	registerKeyword("id", [this](const std::string& idType, std::istream& theStream)
-		{
-			const ID theID(theStream);
-			leaderID = theID.getIDNum();
-		});
+	registerKeyword("name", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleString nameString(theStream);
+		name = nameString.getString();
+	});
+	registerKeyword("type", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleString typeString(theStream);
+		leaderType = typeString.getString();
+	});
+	registerKeyword("female", [this](const std::string& unused, std::istream& theStream) {
+		commonItems::ignoreItem(unused, theStream);
+		female = true;
+	});
+	registerKeyword("manuever", [this](const std::string& unused, std::istream& theStream) { // don't fix PDX typo!
+		const commonItems::singleInt theValue(theStream);
+		maneuver = theValue.getInt();
+	});
+	registerKeyword("fire", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleInt theValue(theStream);
+		fire = theValue.getInt();
+	});
+	registerKeyword("shock", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleInt theValue(theStream);
+		shock = theValue.getInt();
+	});
+	registerKeyword("siege", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleInt theValue(theStream);
+		siege = theValue.getInt();
+	});
+	registerKeyword("country", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleString countryString(theStream);
+		country = countryString.getString();
+	});
+	registerKeyword("activation", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleString dateString(theStream);
+		activationDate = date(dateString.getString());
+	});
+	registerKeyword("id", [this](const std::string& idType, std::istream& theStream) {
+		const ID theID(theStream);
+		leaderID = theID.getIDNum();
+	});
 	registerRegex("[a-zA-Z0-9_\\.:]+", commonItems::ignoreItem);
 
 	parseStream(theStream);
@@ -63,8 +53,10 @@ EU4::Leader::Leader(std::istream& theStream)
 
 bool EU4::Leader::isLand() const
 {
-	if (leaderType == "general" || leaderType == "conquistador") return true;
-	if (leaderType == "explorer" || leaderType == "admiral") return false;
+	if (leaderType == "general" || leaderType == "conquistador")
+		return true;
+	if (leaderType == "explorer" || leaderType == "admiral")
+		return false;
 
 	LOG(LogLevel::Warning) << "Unknown leader type " << leaderType;
 	return false;
