71286aeb06ef58afa6f92ecb7af4ec305f3cee4a
Ivo Elezović <ivo@elezovic.su>
2020-05-26 00:11:30 +0200

    Parser and x64 (#505)
    * updating fronter
    
    * to x64

--- EU4ToVic2Tests/EU4WorldTests/ProvincesTests.cpp
+++ EU4ToVic2Tests/EU4WorldTests/ProvincesTests.cpp
@@ -1,26 +1,3 @@
-/*Copyright (c) 2019 The Paradox Game Converters Project
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
-
-
-
 #include "gtest/gtest.h"
 #include "../EU4toV2/Source/EU4World/Provinces/Provinces.h"
 #include "../EU4toV2/Source/Mappers/ProvinceMappings/ProvinceMapper.h"
@@ -42,9 +19,7 @@ TEST(EU4World_ProvincesTests, provincesCanBeInput)
 {
 	std::stringstream input;
 	input << "={\n";
-	input << "={\n";
 	input << "-1={}";
-	input << "}\n";
 	input << "}";
 
 	EU4::Provinces theProvinces(input);
@@ -56,9 +31,7 @@ TEST(EU4World_ProvincesTests, gettingNonExsistentProvinceThrowsException)
 {
 	std::stringstream input;
 	input << "={\n";
-	input << "={\n";
 	input << "-1={}";
-	input << "}\n";
 	input << "}";
 
 	EU4::Provinces theProvinces(input);
@@ -70,9 +43,7 @@ TEST(EU4World_ProvincesTests, canGetProvince)
 {
 	std::stringstream input;
 	input << "={\n";
-	input << "={\n";
 	input << "-1={}";
-	input << "}\n";
 	input << "}";
 
 	EU4::Provinces theProvinces(input);
