004148c02e8c037bf11bac806b43967f482aece0
Ivo Elezovic <ivo@elezovic.su>
2020-06-09 13:18:00 +0200

    provinces and regions.

--- EU4toV2/Source/EU4World/Regions/SuperRegions.cpp
+++ EU4toV2/Source/EU4World/Regions/SuperRegions.cpp
@@ -6,7 +6,11 @@ EU4::SuperRegions::SuperRegions(std::istream& theStream)
 	registerRegex("\\w+_superregion", [this](const std::string& sregionName, std::istream& theStream)
 		{
 			const commonItems::stringList regionList(theStream);
-			superRegionMap[sregionName] = regionList.getStrings();
+			for (const auto& region: regionList.getStrings())
+			{
+				if (region == "restrict_charter") continue;
+				superRegionMap[sregionName].emplace_back(region);
+			}
 		});
 	registerRegex("[a-zA-Z0-9\\_.:]+", commonItems::ignoreItem);
 
