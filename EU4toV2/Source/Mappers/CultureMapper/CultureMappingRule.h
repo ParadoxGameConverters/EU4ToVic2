6f70ac420bee1cdfd28a18ae39fb52f8c3c41a46
Ivo Elezovic <ivo@elezovic.su>
2020-06-09 22:48:03 +0200

    typos.

--- EU4toV2/Source/Mappers/CultureMapper/CultureMappingRule.h
+++ EU4toV2/Source/Mappers/CultureMapper/CultureMappingRule.h
@@ -4,48 +4,46 @@
 #include "Parser.h"
 #include <set>
 
-namespace EU4 {
-	class Regions;
+namespace EU4
+{
+class Regions;
 }
 
 namespace mappers
 {
-	class CultureMappingRule: commonItems::parser
-	{
-	public:
-		CultureMappingRule() = default;
-		explicit CultureMappingRule(std::istream& theStream);
-		CultureMappingRule(const std::string& v2Culture, const std::string& eu4Culture, const std::string& eu4SuperRegion);
-		
-		[[nodiscard]] std::optional<std::string> cultureMatch(
-			const EU4::Regions& eu4Regions,
-			const std::string& eu4culture,
-			const std::string& eu4religion,
-			int eu4Province,
-			const std::string& eu4ownerTag) const;
+class CultureMappingRule: commonItems::parser
+{
+  public:
+	CultureMappingRule() = default;
+	explicit CultureMappingRule(std::istream& theStream);
+	CultureMappingRule(const std::string& v2Culture, const std::string& eu4Culture, const std::string& eu4SuperRegion);
 
-		[[nodiscard]] std::optional<std::string> cultureRegionalMatch(
-			const EU4::Regions& eu4Regions,
-			const std::string& eu4culture,
-			const std::string& eu4religion,
-			int eu4Province,
-			const std::string& eu4ownerTag) const;
+	[[nodiscard]] std::optional<std::string> cultureMatch(const EU4::Regions& eu4Regions,
+		 const std::string& eu4culture,
+		 const std::string& eu4religion,
+		 int eu4Province,
+		 const std::string& eu4ownerTag) const;
 
-		[[nodiscard]] std::optional<std::string> cultureNonRegionalNonReligiousMatch(
-			const EU4::Regions& eu4Regions,
-			const std::string& eu4culture,
-			const std::string& eu4religion,
-			int eu4Province,
-			const std::string& eu4ownerTag) const;
+	[[nodiscard]] std::optional<std::string> cultureRegionalMatch(const EU4::Regions& eu4Regions,
+		 const std::string& eu4culture,
+		 const std::string& eu4religion,
+		 int eu4Province,
+		 const std::string& eu4ownerTag) const;
 
-	private:
-		std::string destinationCulture;
-		std::set<std::string> cultures;
-		std::set<std::string> religions;
-		std::set<std::string> regions;
-		std::set<std::string> owners;
-		std::set<int> provinces;
-	};
-}
+	[[nodiscard]] std::optional<std::string> cultureNonRegionalNonReligiousMatch(const EU4::Regions& eu4Regions,
+		 const std::string& eu4culture,
+		 const std::string& eu4religion,
+		 int eu4Province,
+		 const std::string& eu4ownerTag) const;
+
+  private:
+	std::string destinationCulture;
+	std::set<std::string> cultures;
+	std::set<std::string> religions;
+	std::set<std::string> regions;
+	std::set<std::string> owners;
+	std::set<int> provinces;
+};
+} // namespace mappers
 
 #endif // CULTURE_MAPPING_RULE_H
