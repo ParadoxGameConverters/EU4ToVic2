3e80ac7226149d0044e192fe4d45d6e326dc05f5
Ivo Elezović <ivo@elezovic.su>
2020-07-01 21:15:33 +0200

    Fixes #524 (#525)

--- EU4toV2/Source/Mappers/CountryMappings/CountryMapping.cpp
+++ EU4toV2/Source/Mappers/CountryMappings/CountryMapping.cpp
@@ -3,29 +3,25 @@
 
 mappers::CountryMapping::CountryMapping(std::istream& theStream)
 {
-	registerKeyword("EU4", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleString eu4str(theStream);
-			eu4Tag = eu4str.getString();
-			std::transform(eu4Tag.begin(), eu4Tag.end(), eu4Tag.begin(), ::toupper);
-		});
-	registerKeyword("Vic2", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleString v2str(theStream);
-			vic2Tag = v2str.getString();
-			std::transform(vic2Tag.begin(), vic2Tag.end(), vic2Tag.begin(), ::toupper);
-		});
-	registerKeyword("reform", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleString reformStr(theStream);
-			reforms.insert(reformStr.getString());
-		});
-	registerKeyword("flag", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleString flagStr(theStream);
-			flags.insert(flagStr.getString());
-		});
-	registerRegex("[a-zA-Z0-9_\\.:]+", commonItems::ignoreItem);
+	registerKeyword("eu4", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleString eu4str(theStream);
+		eu4Tag = eu4str.getString();
+		std::transform(eu4Tag.begin(), eu4Tag.end(), eu4Tag.begin(), ::toupper);
+	});
+	registerKeyword("v2", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleString v2str(theStream);
+		vic2Tag = v2str.getString();
+		std::transform(vic2Tag.begin(), vic2Tag.end(), vic2Tag.begin(), ::toupper);
+	});
+	registerKeyword("reform", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleString reformStr(theStream);
+		reforms.insert(reformStr.getString());
+	});
+	registerKeyword("flag", [this](const std::string& unused, std::istream& theStream) {
+		const commonItems::singleString flagStr(theStream);
+		flags.insert(flagStr.getString());
+	});
+	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
 
 	parseStream(theStream);
 	clearRegisteredKeywords();
