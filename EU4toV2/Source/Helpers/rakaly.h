ee4225cee1f98899ebf680beda0e3b305e2d2535
Nick Babcock <nbabcock19@hotmail.com>
2020-07-25 03:46:38 -0500

    Bump librakaly to v0.3 (#537)
    Changelog
    
    - Instead of raising an error on unknown binary tokens found in ironman saves,
      skip these values if the tokens are part of an object's key, else write out
      an "__unknown_0x" string.
    - Write only one checksum instance per melt operation (previously melting a
      zip would result in each file contributing a checksum in the output).
    - Remove limit to how nested the binary parser can recurse. This will allow
      rakaly to melt modded saves that rely heavily on recursive events.
    
    This was able to successfully melt a user's save who received an error
    due to a unknown binary key (id: 50)

--- EU4toV2/Source/Helpers/rakaly.h
+++ EU4toV2/Source/Helpers/rakaly.h
@@ -10,8 +10,9 @@
  *  that has already been unzipped.
  *  - data_len: Length of the data indicated by the data pointer. It is undefined behavior if the
  *  given length does not match the actual length of the data
- *  - out: Mutable pointer to data which will be filled with the plaintext savefile. Will not end
- *  in a null terminator. The encoding of the plaintext is not strictly defined, rakaly will dump
+ *  - out: Mutable pointer to data which will be filled with the plaintext savefile. Will end
+ *  in a null terminator (but the null terminator is not counted as part of the length).
+ *  The encoding of the plaintext is not strictly defined, rakaly will dump
  *  whatever data is found as strings in the binary data such that they are bit for bit
  *  compatible. While this could mean that string data could be have a different encoding from the
  *  rest of the file, in practice only windows-1252 encoding has been seen for EU4 ironman saves.
@@ -22,11 +23,17 @@
  *
  *  - An early EOF
  *  - Invalid format encountered
- *  - An unresolved key (which means that rakaly needs to be updated to the latest patch)
- *  - Too many open delimiters
  *  - Too many close delimiters
  *
+ * If an unknown token is encountered and rakaly doesn't know how to convert it to plaintext there
+ * are two possible outcomes:
+ *
+ *  - If the token is part of an object's key then key and value will not appear in the plaintext
+ *  output
+ *  - Else the object value (or array value) will be string of "__unknown_x0$z" where z is the
+ *  hexadecimal representation of the unknown token.
+ *
  * A future improvement should allow a client to expose the exact error message or expose the
- * option to ignore unknown keys instead of returning an error.
+ * option to custom behavior on unknown tokens.
  */
 char rakaly_eu4_melt(const char *data_ptr, size_t data_len, char **out, size_t *out_len);
