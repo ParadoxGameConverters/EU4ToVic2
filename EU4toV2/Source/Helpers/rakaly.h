05742a6ce17c740a968793434f549789d0ce2229
D.L. Buunk <dlbuunk@gmail.com>
2020-09-04 13:50:39 +0200

    Updated librakaly: C++ wrapper and fix for memory leak.

--- EU4toV2/Source/Helpers/rakaly.h
+++ EU4toV2/Source/Helpers/rakaly.h
@@ -1,6 +1,15 @@
+#include "stddef.h"
+
+/**
+ * A MeltedBuffer holds the result of a melting operation (binary to plaintext translation).
+ * Either the melting operation succeeded, and the buffer is filled with plaintext or it contains
+ * an error.
+ */
+typedef struct MeltedBuffer MeltedBuffer;
+
 /**
  * Melts binary encoded ironman data into normal plaintext data that can be understood by EU4
- * natively.
+ * natively. The melted buffer, when written out will contain windows-1252 encoded plaintext.
  *
  * Parameters:
  *
@@ -10,20 +19,6 @@
  *  that has already been unzipped.
  *  - data_len: Length of the data indicated by the data pointer. It is undefined behavior if the
  *  given length does not match the actual length of the data
- *  - out: Mutable pointer to data which will be filled with the plaintext savefile. Will end
- *  in a null terminator (but the null terminator is not counted as part of the length).
- *  The encoding of the plaintext is not strictly defined, rakaly will dump
- *  whatever data is found as strings in the binary data such that they are bit for bit
- *  compatible. While this could mean that string data could be have a different encoding from the
- *  rest of the file, in practice only windows-1252 encoding has been seen for EU4 ironman saves.
- *  - out_len: Number of elements now contained in the out pointer.
- *
- * This function will return non-zero to indicate an error. A non-zero status code can occur from
- * the following:
- *
- *  - An early EOF
- *  - Invalid format encountered
- *  - Too many close delimiters
  *
  * If an unknown token is encountered and rakaly doesn't know how to convert it to plaintext there
  * are two possible outcomes:
@@ -32,8 +27,56 @@
  *  output
  *  - Else the object value (or array value) will be string of "__unknown_x0$z" where z is the
  *  hexadecimal representation of the unknown token.
- *
- * A future improvement should allow a client to expose the exact error message or expose the
- * option to custom behavior on unknown tokens.
  */
-char rakaly_eu4_melt(const char *data_ptr, size_t data_len, char **out, size_t *out_len);
+MeltedBuffer *rakaly_eu4_melt(const char *data_ptr, size_t data_len);
+
+/**
+ * Destroys a `MeltedBuffer` once you are done with it.
+ *
+ * # Safety
+ *
+ * Must pass in a valid pointer to a `MeltedBuffer`
+ */
+void rakaly_free_melt(MeltedBuffer *res);
+
+/**
+ * Returns the length of the melted data in bytes. Length excludes null terminator if present,
+ * so make sure you add 1 to this result to ensure a buffer big enough to hold the data is
+ * allocated
+ *
+ * # Safety
+ *
+ * Must pass in a valid pointer to a `MeltedBuffer`
+ */
+size_t rakaly_melt_data_length(const MeltedBuffer *res);
+
+/**
+ * A non-zero return value indicates an error with the melted buffer
+ *
+ * A non-zero status code can occur from the following:
+ *
+ *  - An early EOF
+ *  - Invalid format encountered
+ *  - Too many close delimiters
+ *
+ * # Safety
+ *
+ * Must pass in a valid pointer to a `MeltedBuffer`
+ */
+int rakaly_melt_error_code(const MeltedBuffer *res);
+
+/**
+ * Writes the melted data into a provided buffer that is a given length.
+ *
+ * Returns the number of bytes copied from the melted data to the provided buffer.
+ *
+ * If the buffer is not long enough for the melted data, then -1 is returned.
+ *
+ * If the melted data or provided buffer are null, then -1 is returned.
+ *
+ * # Safety
+ *
+ * - Must pass in a valid pointer to a `MeltedBuffer`
+ * - Given buffer must be at least the given length in size
+ */
+int rakaly_melt_write_data(const MeltedBuffer *res, char *buffer, size_t length);
