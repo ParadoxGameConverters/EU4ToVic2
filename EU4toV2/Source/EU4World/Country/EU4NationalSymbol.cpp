caffb01d6eee176facd1b1ef64db3b630bb064dc
Ivo Elezović <ivo@elezovic.su>
2020-09-07 10:15:53 +0200

    Recolor 2: Electric Boogaloo (#559)

--- EU4toV2/Source/EU4World/Country/EU4NationalSymbol.cpp
+++ EU4toV2/Source/EU4World/Country/EU4NationalSymbol.cpp
@@ -4,13 +4,13 @@
 EU4::NationalSymbol::NationalSymbol(std::istream& theStream)
 {
 	registerKeyword("map_color", [this](const std::string& unused, std::istream& theStream) {
-		mapColor = commonItems::Color::Factory::getColor(theStream);
+		mapColor = commonItems::Color::Factory{}.getColor(theStream);
 	});
 	registerKeyword("revolutionary_colors", [this](const std::string& unused, std::istream& theStream) {
-		revolutionaryColor = commonItems::Color::Factory::getColor(theStream);
+		revolutionaryColor = commonItems::Color::Factory{}.getColor(theStream);
 	});
 	registerKeyword("country_color", [this](const std::string& unused, std::istream& theStream) {
-		countryColor = commonItems::Color::Factory::getColor(theStream);
+		countryColor = commonItems::Color::Factory{}.getColor(theStream);
 	});
 	registerKeyword("custom_colors", [this](const std::string& colorsString, std::istream& theStream) {
 		const CustomColors theSection(theStream);
