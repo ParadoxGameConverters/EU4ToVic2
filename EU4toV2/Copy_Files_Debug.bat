3e33a2f8e8f977a048202a4252afea33fee94011
Ivo Elezovic <ivo@elezovic.su>
2020-10-07 15:32:40 +0200

    cleaning up solution and debug build

--- EU4toV2/Copy_Files_Debug.bat
+++ EU4toV2/Copy_Files_Debug.bat
@@ -1,25 +1,34 @@
 echo on
 rem Copy converter data files
-copy "Data_Files\configuration.txt" "..\Debug\EU4toVic2\configuration.txt"
-copy "Data_Files\ReadMe.txt" "..\Debug\EU4toVic2\readme.txt"
-copy "Data_Files\ReadMe.txt" "..\Debug\readme.txt"
-copy "Data_Files\FAQ.txt" "..\Debug\EU4toVic2\FAQ.txt"
-copy "Data_Files\Eu4ToVic2DefaultConfiguration.xml" "..\Debug\Configuration\Eu4ToVic2DefaultConfiguration.xml"
-copy "Data_Files\SupportedConvertersDefault.xml" "..\Debug\Configuration\SupportedConvertersDefault.xml"
-copy "Data_Files\license.txt" "..\Debug\EU4toVic2\license.txt"
-copy "Data_Files\after_converting.txt" "..\Debug\EU4toVic2\after_converting.txt"
+copy "Data_Files\log.txt" "..\Debug\EU4ToVic2\"
+copy "Data_Files\configuration-example.txt" "..\Debug\EU4ToVic2\"
+copy "Resources\msvcp140_codecvt_ids.dll" "..\Debug\EU4ToVic2\"
+copy "Resources\vcruntime140_1.dll" "..\Debug\EU4ToVic2\"
+copy "Resources\VC_redist.x64.exe" "..\Debug\EU4ToVic2\"
+copy "Resources\rakaly.dll" "..\Debug\EU4ToVic2\"
+
+mkdir "..\Debug\Configuration"
+copy "Data_Files\fronter-configuration.txt" "..\Debug\Configuration\"
+copy "Data_Files\fronter-options.txt" "..\Debug\Configuration\"
+copy "Data_Files\*.yml" "..\Debug\Configuration\"
+
+mkdir "..\Debug\Docs"
+copy "Data_Files\ReadMe.txt" "..\Debug\Docs\"
+copy "Data_Files\FAQ.txt" "..\Debug\Docs\"
+copy "Data_Files\license.txt" "..\Debug\Docs\"
+copy "Data_Files\after_converting.txt" "..\Debug\Docs\"
 
 rem Create Configurables
-del "..\Debug\EU4toVic2\configurables" /Q
-rmdir "..\Debug\EU4toVic2\configurables" /S /Q
-xcopy "Data_Files\configurables" "..\Debug\EU4toVic2\configurables" /Y /E /I
+del "..\Debug\EU4ToVic2\configurables" /Q
+rmdir "..\Debug\EU4ToVic2\configurables" /S /Q
+xcopy "Data_Files\configurables" "..\Debug\EU4ToVic2\configurables" /Y /E /I
 
 rem Create Blank Mod
-del "..\Debug\EU4toVic2\blankMod" /Q
-rmdir "..\Debug\EU4toVic2\blankMod" /S /Q
-xcopy "Data_Files\blankMod" "..\Debug\EU4toVic2\blankMod" /Y /E /I
+del "..\Debug\EU4ToVic2\blankMod" /Q
+rmdir "..\Debug\EU4ToVic2\blankMod" /S /Q
+xcopy "Data_Files\blankMod" "..\Debug\EU4ToVic2\blankMod" /Y /E /I
 
 rem Copy Flags
-del "..\Debug\EU4toVic2\flags" /Q
-rmdir "..\Debug\EU4toVic2\flags" /S /Q
-xcopy "Data_Files\flags" "..\Debug\EU4toVic2\flags" /Y /E /I
+del "..\Debug\EU4ToVic2\flags" /Q
+rmdir "..\Debug\EU4ToVic2\flags" /S /Q
+xcopy "Data_Files\flags" "..\Debug\EU4ToVic2\flags" /Y /E /I
