420190403a438618973cee6eed214fca31cf2d3d
Zemurin <ivo@elezovic.su>
2020-10-24 15:03:45 +0200

    dropping resettables.

--- EU4toV2/Source/Mappers/ProvinceMappings/ProvinceMapper.cpp
+++ EU4toV2/Source/Mappers/ProvinceMappings/ProvinceMapper.cpp
@@ -1,12 +1,12 @@
 #include "ProvinceMapper.h"
-#include "ProvinceMappingsVersion.h"
 #include "../../Configuration.h"
 #include "GameVersion.h"
 #include "Log.h"
+#include "ParserHelpers.h"
+#include "ProvinceMappingsVersion.h"
+#include <filesystem>
 #include <fstream>
 #include <stdexcept>
-#include "ParserHelpers.h"
-#include <filesystem>
 namespace fs = std::filesystem;
 
 mappers::ProvinceMapper::ProvinceMapper()
@@ -32,12 +32,11 @@ mappers::ProvinceMapper::ProvinceMapper(std::istream& theStream, const Configura
 
 void mappers::ProvinceMapper::registerKeys()
 {
-	registerRegex("[0-9\\.]+", [this](const std::string& versionString, std::istream& theStream)
-		{
-			ProvinceMappingsVersion version(versionString, theStream);
-			mappingVersions.insert(std::make_pair(version.getVersion(), version));
-		});
-	registerRegex("[a-zA-Z0-9\\_.:]+", commonItems::ignoreItem);
+	registerRegex(R"([0-9\.]+)", [this](const std::string& versionString, std::istream& theStream) {
+		ProvinceMappingsVersion version(versionString, theStream);
+		mappingVersions.insert(std::make_pair(version.getVersion(), version));
+	});
+	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
 }
 
 bool mappers::ProvinceMapper::provinceIsInRegion(int province, const std::string& region) const
@@ -45,7 +44,8 @@ bool mappers::ProvinceMapper::provinceIsInRegion(int province, const std::string
 	return colonialRegionsMapper.provinceIsInRegion(province, region);
 }
 
-mappers::ProvinceMappingsVersion mappers::ProvinceMapper::getMappingsVersion(const std::map<GameVersion, ProvinceMappingsVersion>& mappingsVersions, const GameVersion& newVersion)
+mappers::ProvinceMappingsVersion mappers::ProvinceMapper::getMappingsVersion(const std::map<GameVersion, ProvinceMappingsVersion>& mappingsVersions,
+	 const GameVersion& newVersion)
 {
 	for (auto mappingsVersion = mappingsVersions.rbegin(); mappingsVersion != mappingsVersions.rend(); ++mappingsVersion)
 	{
@@ -54,7 +54,7 @@ mappers::ProvinceMappingsVersion mappers::ProvinceMapper::getMappingsVersion(con
 			LOG(LogLevel::Info) << "\t-> Using version " << mappingsVersion->first << " mappings";
 			return mappingsVersion->second;
 		}
-	}	
+	}
 	throw std::range_error("Could not find matching province mappings for EU4 version");
 }
 
@@ -63,43 +63,20 @@ void mappers::ProvinceMapper::createMappings(const ProvinceMappingsVersion& prov
 	for (const auto& mapping: provinceMappingsVersion.getMappings())
 	{
 		// fix deliberate errors where we leave mappings without keys (asian wasteland comes to mind):
-		if (mapping.getVic2Provinces().empty()) continue;
-		if (mapping.getEU4Provinces().empty()) continue;
-		
+		if (mapping.getVic2Provinces().empty())
+			continue;
+		if (mapping.getEU4Provinces().empty())
+			continue;
+
 		for (const auto& vic2Number: mapping.getVic2Provinces())
 		{
-			if (vic2Number) vic2ToEU4ProvinceMap.insert(std::make_pair(vic2Number, mapping.getEU4Provinces()));
+			if (vic2Number)
+				vic2ToEU4ProvinceMap.insert(std::make_pair(vic2Number, mapping.getEU4Provinces()));
 		}
 		for (const auto& eu4Number: mapping.getEU4Provinces())
 		{
-			if (eu4Number) eu4ToVic2ProvinceMap.insert(std::make_pair(eu4Number, mapping.getVic2Provinces()));
-		}
-		for (const auto& resettableRegion: mapping.getResettableRegions())
-		{
-			addProvincesToResettableRegion(resettableRegion, mapping.getVic2Provinces());
-		}
-	}
-}
-
-void mappers::ProvinceMapper::addProvincesToResettableRegion(
-	const std::string& regionName,
-	const std::vector<int>& provinces
-) {
-	auto region = resettableProvinces.find(regionName);
-	if (region == resettableProvinces.end())
-	{
-		std::set<int> provincesSet;
-		for (const auto& province : provinces)
-		{
-			provincesSet.insert(province);
-		}
-		resettableProvinces.insert(std::make_pair(regionName, provincesSet));
-	}
-	else
-	{
-		for (const auto& province: provinces)
-		{
-			region->second.insert(province);
+			if (eu4Number)
+				eu4ToVic2ProvinceMap.insert(std::make_pair(eu4Number, mapping.getVic2Provinces()));
 		}
 	}
 }
@@ -107,37 +84,32 @@ void mappers::ProvinceMapper::addProvincesToResettableRegion(
 std::vector<int> mappers::ProvinceMapper::getVic2ProvinceNumbers(const int eu4ProvinceNumber) const
 {
 	const auto& mapping = eu4ToVic2ProvinceMap.find(eu4ProvinceNumber);
-	if (mapping != eu4ToVic2ProvinceMap.end()) return mapping->second;
+	if (mapping != eu4ToVic2ProvinceMap.end())
+		return mapping->second;
 	return std::vector<int>();
 }
 
 std::vector<int> mappers::ProvinceMapper::getEU4ProvinceNumbers(int vic2ProvinceNumber) const
 {
 	const auto& mapping = vic2ToEU4ProvinceMap.find(vic2ProvinceNumber);
-	if (mapping != vic2ToEU4ProvinceMap.end()) return mapping->second;
+	if (mapping != vic2ToEU4ProvinceMap.end())
+		return mapping->second;
 	return std::vector<int>();
-}
-
-bool mappers::ProvinceMapper::isProvinceResettable(int vic2ProvinceNumber, const std::string& region) const
-{
-	const auto& regionProvinces = resettableProvinces.find(region);
-	if (regionProvinces == resettableProvinces.end()) return false;
-	return regionProvinces->second.count(vic2ProvinceNumber) > 0;
 }
 
 void mappers::ProvinceMapper::determineValidProvinces()
 {
 	std::ifstream definitionFile(fs::u8path(theConfiguration.getEU4Path() + "/map/definition.csv"));
-	if (!definitionFile.is_open()) throw std::runtime_error("Could not open <eu4>/map/definition.csv");
+	if (!definitionFile.is_open())
+		throw std::runtime_error("Could not open <eu4>/map/definition.csv");
 
 	char input[256];
 	while (!definitionFile.eof())
 	{
 		definitionFile.getline(input, 255);
 		std::string inputStr(input);
-		if (inputStr.substr(0, 8) == "province" ||
-			inputStr.substr(inputStr.find_last_of(';') + 1, 6) == "Unused" ||
-			inputStr.substr(inputStr.find_last_of(';') + 1, 3) == "RNW")
+		if (inputStr.substr(0, 8) == "province" || inputStr.substr(inputStr.find_last_of(';') + 1, 6) == "Unused" ||
+			 inputStr.substr(inputStr.find_last_of(';') + 1, 3) == "RNW")
 		{
 			continue;
 		}
--- EU4toV2/Source/Mappers/ProvinceMappings/ProvinceMapping.h
+++ EU4toV2/Source/Mappers/ProvinceMappings/ProvinceMapping.h
@@ -6,20 +6,20 @@
 
 namespace mappers
 {
-	class ProvinceMapping: commonItems::parser
-	{
-	public:
-		explicit ProvinceMapping(std::istream& theStream);
+class ProvinceMapping: commonItems::parser
+{
+  public:
+	explicit ProvinceMapping(std::istream& theStream);
 
-		[[nodiscard]] const auto& getEU4Provinces() const { return eu4Provinces; }
-		[[nodiscard]] const auto& getVic2Provinces() const { return vic2Provinces; }
-		[[nodiscard]] const auto& getResettableRegions() const { return resettableRegions; }
+	[[nodiscard]] const auto& getEU4Provinces() const { return eu4Provinces; }
+	[[nodiscard]] const auto& getVic2Provinces() const { return vic2Provinces; }
 
-	private:
-		std::vector<int> eu4Provinces;
-		std::vector<int> vic2Provinces;
-		std::set<std::string> resettableRegions;
-	};
-}
+  private:
+	void registerKeys();
+
+	std::vector<int> eu4Provinces;
+	std::vector<int> vic2Provinces;
+};
+} // namespace mappers
 
 #endif // PROVINCE_MAPPING_H
