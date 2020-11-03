c3299f3f2b016280cb4394d52927e590491b8b7d
Ivo Elezovic <ivo@elezovic.su>
2020-11-03 10:17:20 +0100

    final fix

--- EU4ToVic2Tests/EU4WorldTests/LeaderTests/EU4LeaderIDTests.cpp
+++ EU4ToVic2Tests/EU4WorldTests/LeaderTests/EU4LeaderIDTests.cpp
@@ -1,12 +1,12 @@
 #include "Leader/EU4LeaderID.h"
 #include "gtest/gtest.h"
 
-TEST(EU4World_EU4LeaderIDTests, primitivesDefaultToBlank)
+TEST(EU4World_EU4LeaderIDTests, primitivesDefaultToZero)
 {
 	std::stringstream input;
 	const EU4::LeaderID id(input);
 
-	ASSERT_FALSE(id.getIDNum());
+	ASSERT_EQ(0, id.getIDNum());
 }
 
 TEST(EU4World_EU4LeaderIDTests, idCanBeSet)
