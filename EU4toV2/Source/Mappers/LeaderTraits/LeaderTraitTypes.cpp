040681884ad86cee71f0c222e3417d672729af2e
Ivo Elezovic <ivo@elezovic.su>
2020-11-27 10:56:17 +0100

    leader traits

--- EU4toV2/Source/Mappers/LeaderTraits/LeaderTraitTypes.cpp
+++ EU4toV2/Source/Mappers/LeaderTraits/LeaderTraitTypes.cpp
@@ -1,16 +1,17 @@
 #include "LeaderTraitTypes.h"
 #include "ParserHelpers.h"
-#include "Log.h"
 
 mappers::LeaderTraitTypes::LeaderTraitTypes(std::istream& theStream)
 {
-	registerRegex("[a-z_]+", [this](const std::string& traitName, std::istream& theStream)
-		{
-			const LeaderTrait traitBlock(theStream);
-			traits.emplace_back(std::make_pair(traitName, traitBlock));
-		});
-	registerRegex("[a-zA-Z0-9\\_.:]+", commonItems::ignoreItem);
-
+	registerKeys();
 	parseStream(theStream);
 	clearRegisteredKeywords();
+}
+
+void mappers::LeaderTraitTypes::registerKeys()
+{
+	registerRegex(commonItems::catchallRegex, [this](const std::string& traitName, std::istream& theStream) {
+		const LeaderTrait traitBlock(theStream, traitName);
+		traits.emplace_back(traitBlock);
+	});
 }
