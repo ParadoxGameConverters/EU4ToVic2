b2fe4d7cecfa8fe19c663264b3ddd32bf0f7b941
Ivo Elezovic <ivo@elezovic.su>
2021-01-02 18:04:17 +0100

    Also RegimentCostsMapper for no good reason.

--- EU4toV2/Source/Mappers/RegimentCosts/RegimentCostsMapper.cpp
+++ EU4toV2/Source/Mappers/RegimentCosts/RegimentCostsMapper.cpp
@@ -1,6 +1,6 @@
 #include "RegimentCostsMapper.h"
-#include "ParserHelpers.h"
 #include "Log.h"
+#include "ParserHelpers.h"
 
 mappers::RegimentCostsMapper::RegimentCostsMapper()
 {
@@ -19,17 +19,17 @@ mappers::RegimentCostsMapper::RegimentCostsMapper(std::istream& theStream)
 
 void mappers::RegimentCostsMapper::registerKeys()
 {
-	registerRegex("[a-z_]+", [this](const std::string& regiment, std::istream& theStream)
-		{
-			const commonItems::singleInt costStr(theStream);
-			regimentCosts.insert(std::make_pair(regiment, costStr.getInt()));
-		});
-	registerRegex("[a-zA-Z0-9\\_.:]+", commonItems::ignoreItem);
+	registerRegex("[a-z_]+", [this](const std::string& regiment, std::istream& theStream) {
+		const commonItems::singleInt costStr(theStream);
+		regimentCosts.insert(std::make_pair(regiment, costStr.getInt()));
+	});
+	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
 }
 
 std::optional<int> mappers::RegimentCostsMapper::getCostForRegimentType(const std::string& regimentType) const
 {
-	const auto& costs = regimentCosts.find(regimentType);
-	if (costs != regimentCosts.end()) return costs->second;
-	return std::nullopt;
+	if (const auto& costs = regimentCosts.find(regimentType); costs != regimentCosts.end())
+		return costs->second;
+	else
+		return std::nullopt;
 }
