a7eb2bf5a826b4933ed8cac7bc1f4f4b369aa78c
cetvrtak <cetvrtak@github.com>
2021-03-30 21:46:16 +0200

    HPM techs, inventions & rebels

--- EU4toV2/Source/Mappers/StartingTechMapper/StartingTech.cpp
+++ EU4toV2/Source/Mappers/StartingTechMapper/StartingTech.cpp
@@ -1,25 +1,29 @@
 #include "StartingTech.h"
+#include "CommonRegexes.h"
+#include "Configuration.h"
 #include "ParserHelpers.h"
 
 mappers::StartingTech::StartingTech(std::istream& theStream)
 {
-	registerKeyword("group", [this](const std::string& unused, std::istream& theStream) 
-		{
-			const commonItems::singleString groupStr(theStream);
-			group = groupStr.getString();
-		});
-	registerKeyword("name", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleString nameStr(theStream);
-			name = nameStr.getString();
-		});
-	registerKeyword("score", [this](const std::string& unused, std::istream& theStream)
-		{
-			const commonItems::singleDouble scoreDbl(theStream);
-			score = scoreDbl.getDouble();
-		});
-	registerRegex("[a-zA-Z0-9\\_.:]+", commonItems::ignoreItem);
+	registerKeyword("group", [this](const std::string& unused, std::istream& theStream) {
+		group = commonItems::singleString(theStream).getString();
+	});
+	registerKeyword("name", [this](const std::string& unused, std::istream& theStream) {
+		name = commonItems::singleString(theStream).getString();
+	});
+	registerKeyword("hpm", [this](const std::string& unused, std::istream& theStream) {
+		hpm = commonItems::singleString(theStream).getString();
+	});
+	registerKeyword("score", [this](const std::string& unused, std::istream& theStream) {
+		score = commonItems::singleDouble(theStream).getDouble();
+	});
+	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
 
 	parseStream(theStream);
 	clearRegisteredKeywords();
+
+	if (theConfiguration.isHpmEnabled() && !hpm.empty())
+	{
+		name = hpm;
+	}
 }
