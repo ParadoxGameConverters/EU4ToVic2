7ca8c15bb15d421a42c77f8e36f214dbeec2527a
Ivo Elezović <ivo@elezovic.su>
2021-09-25 12:11:41 +0200

    Fix wf (#775)
    * Fix wf
    
    * Fix wf

---
+++ build_linux_foreign.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export CC=/usr/bin/gcc-11 &&
+export CXX=/usr/bin/g++-11 &&
+
+rm -rf Release/ &&
+cd Fronter &&
+./build_linux.sh &&
+mv Release ../ &&
+cd ../EU4toV2 &&
+rm -rf build &&
+rm -rf Release-Linux &&
+cmake -H. -Bbuild &&
+cmake --build build -- -j3  &&
+mv Release-Linux ../Release/EU4ToVic2 &&
+cd .. &&
+
+cp EU4toV2/Data_Files/*yml Release/Configuration/ &&
+cp EU4toV2/Data_Files/fronter*txt Release/Configuration/ &&
+
+tar -cjf EU4ToVic2-dev-release.tar.bz2 Release
