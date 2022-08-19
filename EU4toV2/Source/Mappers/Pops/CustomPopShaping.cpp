b018937d62b6a1233af816ba42559f3db2e5a156
Idhrendur <idhrendur@hotmail.com>
2022-08-19 16:35:38 -0700

    Only create finalParser once

--- EU4toV2/Source/Mappers/Pops/CustomPopShaping.cpp
+++ EU4toV2/Source/Mappers/Pops/CustomPopShaping.cpp
@@ -32,23 +32,24 @@ void mappers::CustomPopShaping::registerKeys()
 
 void mappers::CustomPopShaping::popShapeTypesForRegions(std::istream& theStream)
 {
+	parser finalParser;
+	finalParser.registerKeyword("pop_shaping", [this](const std::string& unused, std::istream& theStream) {
+		pop_shaping = commonItems::singleString(theStream).getString();
+	});
+	finalParser.registerKeyword("provinces", [this](const std::string& unused, std::istream& theStream) {
+		std::vector<int> tempVec = commonItems::getInts(theStream);
+		std::set<int> tempSet(tempVec.begin(), tempVec.end());
+		if (!popShapeTypes.contains(pop_shaping))
+			popShapeTypes.emplace(pop_shaping, tempSet);
+		else
+			for (const auto& i: tempSet)
+				popShapeTypes.find(pop_shaping)->second.insert(i);
+	});
+	finalParser.registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
+
 	parser newParser;
-	newParser.registerRegex(commonItems::catchallRegex, [this](const std::string& unused, std::istream& theStream) {
-		parser finalParser;
-		finalParser.registerKeyword("pop_shaping", [this](const std::string& unused, std::istream& theStream) {
-			pop_shaping = commonItems::singleString(theStream).getString();
-		});
-		finalParser.registerKeyword("provinces", [this](const std::string& unused, std::istream& theStream) {
-			std::vector<int> tempVec = commonItems::getInts(theStream);
-			std::set<int> tempSet(tempVec.begin(), tempVec.end());
-			if (!popShapeTypes.contains(pop_shaping))
-				popShapeTypes.emplace(pop_shaping, tempSet);
-			else
-				for (const auto& i: tempSet)
-					popShapeTypes.find(pop_shaping)->second.insert(i);
-		});
+	newParser.registerRegex(commonItems::catchallRegex, [this, &finalParser](const std::string& unused, std::istream& theStream) {
 		finalParser.parseStream(theStream);
-		finalParser.registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
 	});
 	newParser.parseStream(theStream);
 	newParser.registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
