a70a3dd840e74936cfba25d67a810ed6e67190be
Ivo Elezović <ivo@element.hr>
2023-02-04 11:53:42 +0100

    Pch (#972)
    * PCH
    
    * fix folder
    
    * lin pch
    
    * fix run
    
    * fix run

--- build_linux.sh
+++ build_linux.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-export CC=/usr/bin/gcc-11 &&
-export CXX=/usr/bin/g++-11 &&
-
-rm -rf Release/ &&
-cd Fronter &&
-./build_linux.sh &&
-mv Release ../ &&
-cd ../EU4toV2 &&
-rm -rf build &&
-rm -rf Release-Linux &&
+rm -rf Release-linux/ &&
+rm -rf test/ &&
+rm -rf build/ &&
 cmake -H. -Bbuild &&
-cmake --build build -- -j40  &&
-mv Release-Linux ../Release/EU4ToVic2 &&
-cd .. &&
-
-cp EU4toV2/Data_Files/*yml Release/Configuration/ &&
-cp EU4toV2/Data_Files/fronter*txt Release/Configuration/ &&
-
-tar -cjf EU4ToVic2-dev-release.tar.bz2 Release
+cmake --build build -- -j40
